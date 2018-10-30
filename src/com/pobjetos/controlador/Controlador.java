f1b8ec0506917944a230d1983d3cd2c23b0cb33f
damian16 <damian1997rko@gmail.com>
2018-10-30 17:38:09 -0500

    Rama 1

--- src/com/pobjetos/controlador/Controlador.java
+++ src/com/pobjetos/controlador/Controlador.java
@@ -6,7 +6,7 @@ package com.pobjetos.controlador;
  */
 public class Controlador {
     public static void main(String[] args) {
-        System.out.print("master");
+        System.out.print("Rama 1");
     }
 
 }
